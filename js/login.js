bd20aba668f1c0b9054daedc5908c9c5c7404708
ptm119 <normalboy0119@gmail.com>
2023-01-10 19:36:15 +0900

    Create login screen and more
    +index.html
    +css files
    +js files
    +background image

---
+++ js/login.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector(".login-form__input");
+
+const USERNAME_KEY = "jegukUsername"
+
+function saveUsername(username) {
+    localStorage.setItem(USERNAME_KEY, username);
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write your name");
+        loginInput.value = "";
+    } else {
+        saveUsername(username);
+        loginForm.submit();
+    }
+}
+
+if (localStorage.getItem(USERNAME_KEY) === null) {
+    loginForm.addEventListener("submit", handleFormSubmit);
+} else {
+    loginForm.submit();
+}
+
