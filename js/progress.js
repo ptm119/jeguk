b7023596a2b90cf0b8fbbf1fbc426019f1a8ca7f
ptm119 <normalboy0119@gmail.com>
2023-01-13 07:54:22 +0900

    Added delete button and some css

--- js/progress.js
+++ js/progress.js
@@ -2,15 +2,34 @@ const form = document.querySelector(".progress");
 const level = document.querySelector(".level-input");
 const progress = document.querySelector(".progress-input");
 
-function CreateLi(obj) {
+let progressArr = [];
+const PROGRESS_ARR_KEY = "progress-list";
+
+function saveProgressArr() {
+    localStorage.setItem(PROGRESS_ARR_KEY, JSON.stringify(progressArr));
+}
+
+function deleteLi(event) {
+    const li = event.target.parentElement;
+    progressArr = progressArr.filter((item) => item.id !== parseInt(li.id));
+    saveProgressArr();
+    li.remove();
+}
+
+function createLi(obj) {
     const li = document.createElement("li");
+    li.id = obj.id;
     const level = document.createElement("span");
     level.innerText = obj.level;
     const progress = document.createElement("span");
     progress.innerText = `${obj.progress}%`;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete"
+    deleteButton.addEventListener("click", deleteLi);
     const ul = document.querySelector(".progress__list");
     li.appendChild(level);
     li.appendChild(progress);
+    li.appendChild(deleteButton);
     ul.appendChild(li);
 }
 
@@ -20,13 +39,28 @@ function handleFormSubmit(event) {
         alert("Please write level name");
     } else if (progress.value === "") {
         alert("Please write your progress");
-    } else if (parseInt(progress.value) > 100) {
+    } else if (parseInt(progress.value) > 100 || parseInt(progress.value) < 0) {
         alert("Please write your progress again");
     } else {
-        CreateLi({ level: level.value, progress: progress.value });
+        const progressObj = {
+            level: level.value,
+            progress: progress.value,
+            id: Date.now()
+        }
+        createLi(progressObj);
+        progressArr.push(progressObj);
+        saveProgressArr();
         level.value = "";
         progress.value = "";
+
     }
+}
+
+const savedProgressArr = localStorage.getItem(PROGRESS_ARR_KEY);
+
+if (savedProgressArr !== null) {
+    progressArr = JSON.parse(savedProgressArr);
+    progressArr.forEach(createLi);
 }
 
 form.addEventListener("submit", handleFormSubmit);
